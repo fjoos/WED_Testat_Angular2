dcdf47c3f074144b617cb0530620e9a1c4cf6e41
fjoos <fabjoos@gmail.com>
2017-03-30 13:54:33 +0200

    react - gettransactions

--- Testat-Vorlage-React_v1.0.0/src/components/AllTransactions.js
+++ Testat-Vorlage-React_v1.0.0/src/components/AllTransactions.js
@@ -1,4 +1,18 @@
 // @flow
+import { getAccountDetails, getAccount, transfer, getTransactions } from '../api'
+
+/*
+ Use the api functions to call the API server. For example, the transactions
+ can be retrieved and stored in the state as follows:
+
+ getTransactions(this.props.token)
+ .then(({result: transactions}) =>
+ this.setState({transactions})
+ )
+
+ import { getAccountDetails, getAccount, transfer, getTransactions } from '../api'
+ */
+
 
 import React from 'react'
 import { Button } from 'semantic-ui-react'
@@ -11,18 +25,29 @@ export type Props = {
 
 class AllTransactions extends React.Component {
   
-  props: Props
-  
+  props: Props;
+
+    state ={
+      transactions: []
+    }
+
   render() {    
     return (
       <div>All Transactions
 
         <Button>asdfaf</Button>
-
+          {this.state.transactions.map(({amount})=>
+          <h1>{amount}</h1>
+          )}
       </div>
-
     )
   }
+    componentDidMount() {
+        getTransactions(this.props.token)
+            .then(({result: transactions}) =>
+                this.setState({transactions})
+            )
+    }
 }
 
 export default AllTransactions
