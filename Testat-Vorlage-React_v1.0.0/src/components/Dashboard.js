0f065c4e2b9258b644e73808c6effda55b2c39e2
DESKTOP-7S04I73\berth <berther_enzo@hotmail.com>
2017-03-30 15:19:11 +0200

    React baby :*

--- Testat-Vorlage-React_v1.0.0/src/components/Dashboard.js
+++ Testat-Vorlage-React_v1.0.0/src/components/Dashboard.js
@@ -2,7 +2,8 @@
 
 import React from 'react'
 import { getAccountDetails, getAccount, transfer, getTransactions } from '../api'
-import { Dropdown } from 'semantic-ui-react'
+import { Dropdown, Grid, Segment } from 'semantic-ui-react'
+
 /*
   Use the api functions to call the API server. For example, the transactions
   can be retrieved and stored in the state as follows:
@@ -24,28 +25,85 @@ class Dashboard extends React.Component {
     state = {
         kontostand: undefined,
         zielKonto: undefined,
-        betrag: 0
+        betrag: 0,
+        transactions: []
+
 
     };
 
   render() {
-    const kontoDescription = this.props.user.accountNr+'('+this.state.kontostand+')';
+    const kontoDescription = this.props.user.accountNr+'(CHF: '+this.state.kontostand+'.-)';
     return (
         <div>
             <header>Kontoübersicht {this.props.user.accountNr}</header>
             <form>
                 <header>Neue Zahlung</header>
-                <label>Von</label>
-                <Dropdown text={kontoDescription}
-                          value={this.props.user.accountNr}
-                          options={[{key:'1', value:this.state.meinKonto, text:kontoDescription}]}
-                          type="text"/>
-                <label>Nach</label>
-                <input value={this.state.zielKonto} type="text" onChange={this.handleTransactionTargetChange} placeholder="Zielkontonummer"/>
-                <label>Betrag</label>
-                <input value={this.state.betrag} type="number" onChange={this.handleAmountChange}/>
+                <div>
+                    <label>Von</label>
+                    <Dropdown text={kontoDescription}
+                              value={this.props.user.accountNr}
+                              options={[{key:'1', value:this.state.meinKonto, text:kontoDescription}]}
+                              type="text"/>
+                </div>
+                <div>
+                    <label>Nach</label>
+                    <input value={this.state.zielKonto}
+                           type="text"
+                           onChange={this.handleTransactionTargetChange}
+                           placeholder="Zielkontonummer"/>
+                </div>
+                <div>
+                    <label>Betrag</label>
+                    <input value={this.state.betrag}
+                           type="number"
+                           onChange={this.handleAmountChange}/>
+                </div>
+                <div>
+                    <button type="submit">Betrag Überweisen</button>
+                </div>
             </form>
-            <button>Betrag Überweisen</button>
+
+            <Segment>
+                <header>Letzte Zahlungen</header>
+                <Grid columns='equal' stackable>
+                    <Grid.Column>
+                    </Grid.Column>
+                    <Grid.Column>
+                        Von
+                    </Grid.Column>
+                    <Grid.Column>
+                        Nach
+                    </Grid.Column>
+                    <Grid.Column>
+                        Betrag
+                    </Grid.Column>
+                    <Grid.Column>
+                        Saldo
+                    </Grid.Column>
+                </Grid>
+                {this.state.transactions.map(({from, target, amount, total, date})=>
+
+                    <Grid columns='equal' stackable>
+                        <Grid.Column>
+                            {date}-
+                        </Grid.Column>
+                        <Grid.Column>
+                            {from}
+                        </Grid.Column>
+                        <Grid.Column>
+                            {target}
+                        </Grid.Column>
+                        <Grid.Column>
+                            {amount}
+                        </Grid.Column>
+                        <Grid.Column>
+                            {total}
+                        </Grid.Column>
+                    </Grid>
+
+                )}
+            </Segment>
+
         </div>
 
 
@@ -54,16 +112,21 @@ class Dashboard extends React.Component {
 
     handleTransactionTargetChange = (event) => {
       this.setState({zielKonto: event.target.value})
-    }
+    };
 
     handleAmountChange = (event) => {
       this.setState({betrag: event.target.value})
-    }
+    };
     componentDidMount() {
         getAccountDetails(this.props.token)
             .then(({amount}) =>
                 this.setState({kontostand: amount})
-            )
+            );
+            getTransactions(this.props.token)
+                .then(({result: transactions}) =>
+                    this.setState({transactions})
+                )
+
     }
 
 
