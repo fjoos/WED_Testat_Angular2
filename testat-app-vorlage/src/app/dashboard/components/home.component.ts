d265fe61769c616145061ca5ff5a4df1265ea203
DESKTOP-7S04I73\berth <berther_enzo@hotmail.com>
2017-03-30 13:36:06 +0200

    React baby :*

---
+++ testat-app-vorlage/src/app/dashboard/components/home.component.ts
@@ -0,0 +1,16 @@
+import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+import {NavigationService} from "../../core/services/navigation.service";
+
+
+@Component({
+  selector: 'homeBoard',
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss']
+})
+
+export class HomeComponent{
+
+}
